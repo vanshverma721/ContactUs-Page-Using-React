0e6cb1c04302663a6b53a24843331ce093b4095e
vanshverma721 <vvv.721781@gmail.com>
2024-01-21 23:08:28 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import ContactHeader from './components/ContactHeader/ContactHeader'
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <ContactHeader />
     </div>
   )
 }
